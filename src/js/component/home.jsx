08de8d668797185746c213794d5fc149a2f99699
Caro72 <106418089+Caro72@users.noreply.github.com>
2023-07-31 20:15:11 +0000

    list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    // PUT
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/caro72', {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        console.log("Tasks synchronized with the server:", data);
+      })
+      .catch(error => {
+        console.error("Error synchronizing tasks:", error);
+      });
+  }, [todos]);
+
+  // Function to add a new task
+  const addTodo = () => {
+    if (newTask.trim() !== "") {
+      const newTodo = {
+        id: Date.now(),
+        label: newTask,
+        done: false,
+      };
+      setTodos([...todos, newTodo]);
+      setNewTask(""); 
+    }
+  };
+
+  // Function to toggle the status of a task (done or not done)
+  const toggleTaskStatus = (taskId) => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === taskId) {
+        return { ...todo, done: !todo.done };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  // Function to remove a task by its id
+  const removeTodo = (taskId) => {
+    const updatedTodos = todos.filter(todo => todo.id !== taskId);
+    setTodos(updatedTodos);
+  };
+
+  // Function to remove all tasks
+  const removeAll = () => {
+    setTodos([]);
+  };
+
+  return (
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <ul className="todo-list">
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.done}
+              onChange={() => toggleTaskStatus(todo.id)}
+            />
+            <span className={todo.done ? "task-done" : ""}>{todo.label}</span>
+          </li>
+        ))}
+      </ul>
+      <div className="add-todo">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter a new task"
+        />
+        <button onClick={addTodo}>Add</button>
+        <button onClick={removeAll}>Remove All</button>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default TodoApp;
+
